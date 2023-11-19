2f1d0619d510438a1b13135813c7d7ed17c5f29b
Vincent Liu <vliu@kpler.com>
2023-11-19 10:35:41 +0800

    fix bash

--- modules/ecs/scripts/ecs_task.sh
+++ modules/ecs/scripts/ecs_task.sh
@@ -4,7 +4,7 @@ set -e
 ECS_TASK_DEFINITION=$1
 REGION=$2
 
-if [[ -z $AWS_PROFILE ]]; then 
+if [[ ! -z $AWS_PROFILE ]]; then 
   IMAGE_NAME=`AWS_PROFILE=$AWS_PROFILE aws ecs describe-task-definition --region ${REGION} --task-definition ${ECS_TASK_DEFINITION} | jq -re '.taskDefinition.containerDefinitions[0].image'`
 else
   IMAGE_NAME=`aws ecs describe-task-definition --region ${REGION} --task-definition ${ECS_TASK_DEFINITION} | jq -re '.taskDefinition.containerDefinitions[0].image'`
