46ad3b2d0645dfb39954b402ce0819cc3e517537
Vincent Liu <vliu@kpler.com>
2023-12-22 16:31:52 +0800

    fix res

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -173,8 +173,8 @@ module "ecs_service" {
   desired_count            = each.value.desired_count
   autoscaling_min_capacity = each.value.enable_autoscaling ? each.value.desired_count : 1
   cluster_arn              = module.ecs_cluster.arn
-  cpu                      = ceil(each.value.container_cpu / 512) * 512
-  memory                   = ceil(each.value.container_memory * 1.1 / 1024) * 1024
+  cpu                      = ceil(each.value.container_cpu / 256) * 256
+  memory                   = ceil(each.value.container_memory / 512) * 512
   enable_autoscaling       = each.value.enable_autoscaling
   enable_execute_command   = true
   task_exec_secret_arns    = each.value.task_exec_secret_arns
