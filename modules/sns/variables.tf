e99262168a3594228976e56ee10b9c01c86813e2
Cris R <42707764+Madgeniusblink@users.noreply.github.com>
2026-07-15 22:24:13 -0700

    fix: preserve SNS subscription identity (#11)

--- modules/sns/variables.tf
+++ modules/sns/variables.tf
@@ -6,6 +6,17 @@ variable "create" {
 variable "sns_email_subscriptions" {
   type    = list(string)
   default = []
+
+  validation {
+    condition = length([
+      for endpoint in var.sns_email_subscriptions : lower(trimspace(endpoint))
+      if trimspace(endpoint) != ""
+      ]) == length(distinct([
+        for endpoint in var.sns_email_subscriptions : lower(trimspace(endpoint))
+        if trimspace(endpoint) != ""
+    ]))
+    error_message = "sns_email_subscriptions must contain unique email addresses, ignoring case and surrounding whitespace."
+  }
 }
 
 variable "webhook_url" {
